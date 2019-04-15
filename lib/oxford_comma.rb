976c5a3395b420664662a7f1438bba42f08edb91
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-15 02:37:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(' and ')
+  elsif
+    array.length == 1
+    array.join
+  else
+    arrayString = array
+    last = array.last
+    last.to_s
+    array.pop
+    string = array.join(', ')
+    "#{string}, and #{last}"
+  end
 end
