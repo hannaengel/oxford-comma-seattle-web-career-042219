5702570ca4905bee58bb0d3196cbd8a4987c1e11
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-15 02:36:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 2
-    array.join('and')
+    array.join(' and ')
+  elsif array.length == 1
+    array.to_s
   else
     arrayString = array
     last = array.last
